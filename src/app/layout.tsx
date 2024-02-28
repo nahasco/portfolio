6297b471dd6215173c81ac7f16ba7a5bba23418b
Mohamed Alnahhas <alnahhas55@gmail.com>
2024-02-28 19:31:06 +0100

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className={inter.className}>
+                <Analytics/>
                 <ThemeProvider attribute="class" forcedTheme="dark" enableSystem disableTransitionOnChange>
                     {children}
                 </ThemeProvider>
